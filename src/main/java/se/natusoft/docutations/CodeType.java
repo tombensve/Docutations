5a22a156f5a213c6eac06dc9b6abb733e589b4e8
tommy <tommy@natusoft.se>
2024-01-07 13:02:29 +0100

    Did a "Refactor rename" on all annotations to change prefix from "DT" to "DOC". "DT" for "DocuTations" wasn't clear enough IMHO.

---
+++ src/main/java/se/natusoft/docutations/CodeType.java
@@ -0,0 +1,5 @@
+package se.natusoft.docutations;
+
+public enum CodeType {
+    Class, Interface, Enum, Method, Field, Member, Constant, Closure, None
+}
---
+++ src/main/java/se/natusoft/docutations/DOC_Disclaimer.java
@@ -0,0 +1,41 @@
+/*
+ *
+ * PROJECT
+ *     Name
+ *         Docutations
+ *     
+ *     Code Version
+ *         2.7
+ *     
+ *     Description
+ *         Provides code documentative annotations.
+ *         
+ * COPYRIGHTS
+ *     Copyright (C) 2015 by Natusoft AB All rights reserved.
+ *     
+ * LICENSE
+ *     ASF 2.0 (Open Source)
+ *     
+ *     
+ * AUTHORS
+ *     tommy ()
+ *         Changes:
+ *         2015-10-24: Created!
+ *
+ */
+package se.natusoft.docutations;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * This annotation is used for special notes.
+ */
+@Documented
+@DOC_Documentative
+@Retention(RetentionPolicy.SOURCE)
+@DOC_Disclaimer("All targets!")
+public @interface DOC_Disclaimer {
+    String[] value() default {};
+}
